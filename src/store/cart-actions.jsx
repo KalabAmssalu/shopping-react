79f8a1c17eaeaff08a75f8b9f7c90c4db7238722
Kalab Amssalu <Kalishdesandy@gmail.com>
2022-12-08 13:59:42 +0300

    cartaction

--- src/store/cart-actions.jsx
+++ src/store/cart-actions.jsx
@@ -1,48 +1,71 @@
 import { cartActions } from "./cart-slice"
 import { uiActions } from "./ui-slice"
-export const fetchData = () => {
-    return async(dispatch) => {
-        const fetchHandler = async() => {
-            const res = await fetch('https://shoppingdbs-6c77c-default-rtdb.firebaseio.com/cartitems.json')
-            const data = await res.json(); 
-            return data;
-        }
-        try{
-            const cartData = await fetchHandler();
-            dispatch(cartActions.replaceData(cartData))
-        }catch(err){
-            dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sending Request To fatch Data Failed",
-                type: "error"
-              }))
-        }
-    }
-}  
-export const sendCartData = (cart) => {
-    return async(dispatch) => {
-        dispatch(uiActions.showNotification({
-            open: true,
-            message: "sending request",
-            type: "warning"
-          }))
+
+export const fetchData=()=>{
+    return async(dispatch)=>{
+      const fetchHandler= async()=>{
+        const res = await fetch("https://shoppingdbs-6c77c-default-rtdb.firebaseio.com/cartitems.json")
+      const data = await res.json()
+      return data;  
     
-          const sendRequest = async() => {
+    }
+    try{
+      const cartData= await fetchHandler();
+      dispatch(cartActions.replaceData(cartData))
+    }
+    catch(err)
+    {
+        dispatch(uiActions.showNotification({
+            open:true,
+            message:'Sending Request failed',
+            type:'error',
+          }))
+    }
+
+
+    }
+}
+
+export const sendCartData = (cart)=>{
+    return async(dispatch)=>{
+        dispatch(
+            uiActions.showNotification({
+            open:true,
+            message:'Sending Request',
+            type:'warning'
+          }))
           
-              dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sent Request To DataBase Successfully",
-                type: "success"
-              }))
-            }
-            try{
-                await sendRequest()
-            }catch (err){
-                dispatch(uiActions.showNotification({
-                    open: true,
-                    message: "Sending Request Failed",
-                    type: "error"
-                  }))
-            }
-        }
+const sendRequest = async()=>{
+   
+    
+  const res = await fetch(
+    
+    "https://shoppingdbs-6c77c-default-rtdb.firebaseio.com/cartitems.json",{
+    method:'PUT',
+    body: JSON.stringify(cart),
+  }
+  
+  );
+  
+  await res.json()
+  dispatch(uiActions.showNotification({
+    open:true,
+    message:'Sent the request to Database Successfully',
+    type:'success'
+    })
+    )
+  }
+  try{
+    await sendRequest();
+  }
+  catch(err)
+  {
+    dispatch(uiActions.showNotification({
+      open:true,
+      message:'Sending Request failed',
+      type:'error',
+    }))
+  }
+    }
+    
 }
